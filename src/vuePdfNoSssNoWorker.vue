2b816cd10e1c3c1f0427ebcbdfdaacd73ae70ccf
Cyvelnet <Cyvelnet@gmail.com>
2022-04-10 14:13:35 +0800

    Update vuePdfNoSssNoWorker.vue

--- src/vuePdfNoSssNoWorker.vue
+++ src/vuePdfNoSssNoWorker.vue
@@ -4,7 +4,7 @@ import componentFactory from "./componentFactory.js";
 
 if (process.env.VUE_ENV !== "server") {
   var pdfjsWrapper = require("./pdfjsWrapper").default;
-  var PDFJS = require("pdfjs-dist");
+  var PDFJS = require("@fanthos/pdfjs-dist-viewer-legacy");
   var component = componentFactory(pdfjsWrapper(PDFJS));
 } else {
   var component = componentFactory({});
